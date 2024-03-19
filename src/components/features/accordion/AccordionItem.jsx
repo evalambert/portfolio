a522bb90f76be55825ef5226d68d963f4fdfc10d
evalambert <145194730+evalambert@users.noreply.github.com>
2024-03-19 14:39:31 +0100

    uddate v2

--- src/components/features/accordion/AccordionItem.jsx
+++ src/components/features/accordion/AccordionItem.jsx
@@ -1,94 +1,176 @@
-//AccordionItem/jsx
-import React from "react";
+//AccordionItem.jsx
+
+import React, { useState, useEffect } from "react";
 import SliderTwo from "../slider/SliderProject";
 import { motion } from "framer-motion";
 
-// Utilisation de React.memo pour optimiser les performances en évitant des re-rendus inutiles
-const AccordionItem = React.memo(({
-  // Déstructuration des props passées au composant
-  number, type, lien, href, title, images, year, content, isOpen, onClick,
-}) => {
+const AccordionItem = React.memo(
+  ({
+    number,
+    type,
+    lien,
+    href,
+    title,
+    images,
+    year,
+    content,
+    isOpen,
+    onClick,
+  }) => {
+    const initialProps = { height: 0 };
+    const [isContentVisible, setIsContentVisible] = useState(isOpen);
 
-  // Propriétés d'animation pour le composant, variant selon que l'item est ouvert ou fermé
-  const animateProps = isOpen ? { maxHeight: "1000px" } : { maxHeight: "0px" };
+    useEffect(() => {
+      let timeoutId;
+      if (isOpen) {
+        setIsContentVisible(true); // Rendre le contenu visible immédiatement si ouvert
+      } else {
+        // Délai 1 seconde pour cacher le contenu après l'animation
+        timeoutId = setTimeout(() => {
+          setIsContentVisible(false);
+        }, 300);
+      }
 
-  // Fonction pour formater le texte avec des parties soulignées basées sur une syntaxe spécifique
-  function formatText(content) {
-    const regex = /(\[\[(.*?)\]\])/g; // Expression régulière pour identifier le texte à formater
-    let parts = [];
-    let lastIndex = 0;
+      return () => clearTimeout(timeoutId);
+    }, [isOpen]);
 
-    // Remplacement du texte correspondant à l'expression régulière par des éléments span avec une bordure
-    content.replace(regex, (match, p1, p2, offset) => {
-      parts.push(content.substring(lastIndex, offset));
-      parts.push(
-        <span className="border-b border-black" key={offset}>
-          {p2}
-        </span>
-      );
-      lastIndex = offset + match.length;
-    });
+    const [contentHeight, setContentHeight] = useState("calc(100vh - 320px)");
 
-    // Ajout du reste du texte après la dernière correspondance
-    if (lastIndex < content.length) {
-      parts.push(content.substring(lastIndex));
+    useEffect(() => {
+      const updateHeight = () => {
+        if (
+          window.matchMedia("(max-width: 1720px) and (max-height: 600px)")
+            .matches
+        ) {
+          setContentHeight("calc(100vh - 270px)");
+        } else if (
+          window.matchMedia("(max-width: 375px) and (max-height: 667px)")
+            .matches // iphone XR
+        ) {
+          setContentHeight("calc(100vh - 290px)");
+        } else if (
+          window.matchMedia("(max-width: 414px) and (max-height: 896px)")
+            .matches // iphone XR
+        ) {
+          setContentHeight("calc(100vh - 500px)");
+        } else if (
+          window.matchMedia("(max-width: 390px) and (max-height: 844px)")
+            .matches // iphone 12 pro 
+        ) {
+          setContentHeight("calc(100vh - 450px)");
+        } else if (
+          window.matchMedia("(max-width: 430px) and (max-height: 932px)")
+            .matches // iphone 14 pro max
+        ) {
+          setContentHeight("calc(100vh - 530px)");
+        } else if (
+          window.matchMedia("(max-width: 360px) and (max-height: 740px)")
+            .matches // samesun s8
+        ) {
+          setContentHeight("calc(100vh - 370px)");
+        } else if (
+          window.matchMedia("(max-width: 412px) and (max-height: 915px)")
+            .matches
+        ) {
+          setContentHeight("calc(100vh - 500px)"); // samsung galaxy ultra
+        } else if (
+          window.matchMedia("(max-width: 768px) and (max-height: 1024px)")
+            .matches // Ipad mini
+        ) {
+          setContentHeight("calc(100vh - 740px)");
+        } else if (
+          window.matchMedia("(max-width: 820px) and (max-height: 1180px)")
+            .matches // Ipad air
+        ) {
+          setContentHeight("calc(100vh - 880px)");
+        } else if (
+          window.matchMedia("(max-width: 1024px) and (max-height: 1366px)")
+            .matches // Ipad pro
+        ) {
+          setContentHeight("calc(100vh - 980px)");
+        } else {
+          setContentHeight("calc(100vh - 320px)");
+        }
+      };
+
+      window.addEventListener("resize", updateHeight);
+      updateHeight();
+
+      return () => window.removeEventListener("resize", updateHeight);
+    }, []);
+
+    const animateProps = isOpen ? { height: contentHeight } : { height: 0 };
+
+    // ++++ Fonction formatage texte souligné ++++
+    function formatText(content) {
+      const regex = /(\[\[(.*?)\]\])/g;
+      let parts = [];
+      let lastIndex = 0;
+
+      content.replace(regex, (match, p1, p2, offset) => {
+        parts.push(content.substring(lastIndex, offset));
+        parts.push(
+          <span className="border-b border-black" key={offset}>
+            {p2}
+          </span>
+        );
+        lastIndex = offset + match.length;
+      });
+
+      if (lastIndex < content.length) {
+        parts.push(content.substring(lastIndex));
+      }
+
+      return <>{parts}</>;
     }
 
-    // Retourne le contenu formaté comme un fragment React
-    return <>{parts}</>;
+    return (
+      <li className="overflow-hidden bg-white">
+        <button
+          aria-expanded={isOpen}
+          className="w-full grid grid-cols-4 border-t-[1px] border-black"
+          onClick={onClick}
+        >
+          <div className="col-span-3 md:col-span-2 grid grid-cols-[20px_1fr] gap-2 md:gap-3">
+            <h2 className="">{number}</h2>
+            <h2 className="text-left truncate overflow-hidden">{title}</h2>
+          </div>
+          <div className="md:col-span-2 grid grid-cols-[1fr_auto] md:gap-4">
+            <h3 className="text-left truncate overflow-hidden hidden md:block">
+              {type}
+            </h3>
+            <h3 className="text-right truncate overflow-hidden">{year}</h3>
+          </div>
+        </button>
+        <motion.article
+          initial={initialProps}
+          animate={animateProps}
+          transition={{ duration: 0.3, ease: "linear" }}
+          className="overflow-scroll md:overflow-hidden bg-white"
+        >
+          {isContentVisible && (
+            <div className="content-work h-full w-full flex flex-col md:flex-row gap-4 py-3">
+              <div className="md:w-1/2 flex flex-col gap-4">
+                <p>{formatText(content)}</p>
+                <p>
+                  <a href={href} target="_blank" rel="noreferrer">
+                    {lien}
+                  </a>
+                </p>
+              </div>
+              <div className="md:w-1/2 md:overflow-hidden">
+                {images && Array.isArray(images) && (
+                  <SliderTwo images={images} />
+                )}
+              </div>
+            </div>
+          )}
+        </motion.article>
+      </li>
+    );
   }
+);
 
-  // Rendu du composant
-  return (
-    <li className="overflow-hidden">
-      <button
-        aria-expanded={isOpen}
-        className="w-full grid grid-cols-4 border-t-[1px] border-black"
-        onClick={onClick}
-      >
-        <div className="col-span-3 md:col-span-2 grid grid-cols-[20px_1fr] gap-2 md:gap-3">
-          <h2>{number}</h2>
-          <h2 className="text-left truncate overflow-hidden">{title}</h2>
-        </div>
-        <div className="md:col-span-2 grid grid-cols-[1fr_auto] md:gap-4">
-          <h3 className="text-left truncate overflow-hidden hidden md:block">{type}</h3>
-          <h3 className="text-right truncate overflow-hidden">{year}</h3>
-        </div>
-      </button>
-      <motion.article
-        initial={{ maxHeight: "0px" }}
-        animate={animateProps} // Applique l'animation de hauteur
-        transition={{ duration: 0.3, ease: "linear" }}
-        className="overflow-hidden"
-      >
-        <div className="content-work h-full w-full flex flex-col md:flex-row gap-4 py-3">
-          <div className="md:w-1/2 flex flex-col gap-4">
-            <p>{formatText(content)}</p>
-            <p>
-              <a
-                className="border-b border-black"
-                href={href}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {lien}
-              </a>
-            </p>
-          </div>
-          <div className="md:w-1/2">
-            {/* Affichage des images via le composant SliderTwo si 'images' est un tableau */}
-            {images && Array.isArray(images) && (
-              <SliderTwo images={images} />
-            )}
-          </div>
-        </div>
-      </motion.article>
-    </li>
-  );
-});
-
-// Définition du nom d'affichage pour le débogage
 AccordionItem.displayName = "AccordionItem";
 
-// Exportation du composant pour une utilisation ailleurs
 export default AccordionItem;
--- src/components/layout/Project.jsx
+++ src/components/layout/Project.jsx
@@ -12,25 +12,20 @@ export default function Project() {
   const [activeSection, setActiveSection] = useState("");
   const [isExiting, setIsExiting] = useState(false);
 
-  // Gère les clics sur les sections: ouvre/ferme les projets et montre/cache le slider.
   const handleSectionClick = (section) => {
     if (activeSection === section) {
-      // Si la section cliquée est déjà active, commence l'animation de sortie.
       setIsExiting(true);
       setTimeout(() => {
-        // Après l'animation, réinitialise les états et montre le slider.
         setActiveSection("");
         setIsExiting(false);
         showSlider();
-      }, 500); // Assure que la durée correspond à celle de l'animation.
+      }, 500);
     } else {
-      // Si une nouvelle section est cliquée, l'active et cache le slider.
       setActiveSection(section);
       hideSlider();
     }
   };
 
-  // Gère la fermeture des projets et montre le slider, similaire à handleSectionClick pour la même section.
   const handleCloseClick = () => {
     setIsExiting(true);
     setTimeout(() => {
@@ -40,36 +35,35 @@ export default function Project() {
     }, 500);
   };
 
-  // Effet pour s'assurer que l'état isExiting est correctement géré lorsqu'aucune section n'est active.
   useEffect(() => {
     if (!activeSection && !isExiting) {
       setIsExiting(false);
     }
   }, [activeSection, isExiting]);
 
-  // Classes conditionnelles pour gérer les bordures et transitions de fond.
   const borderClass = activeSection ? "border-t-[1px]" : "";
-  const backgroundTransitionClass = "transition-background duration-500 ease-in-out";
+  const backgroundTransitionClass =
+    "transition-background duration-500 ease-in-out";
 
-  // La section rendue contient la navigation et le contenu de la section active.
-  // Utilisation de Framer Motion pour les animations de transition.
   return (
     <motion.section
-      key={activeSection} // Clé unique pour le re-rendu lors du changement de section.
-      initial={isExiting ? { y: 0 } : { y: "calc(100vh - 80px)" }} // Position initiale.
-      animate={isExiting ? { y: "calc(100vh - 80px)" } : { y: 0 }} // Animation de sortie ou d'entrée.
+      key={activeSection}
+      initial={isExiting ? { y: 0 } : { y: "calc(100vh - 80px)" }}
+      animate={isExiting ? { y: "calc(100vh - 80px)" } : { y: 0 }}
       className={`w-full absolute px-[10px] md:px-[20px] ${
-        activeSection ? "top-[25px] md:top-[50px] h-screen" : "bottom-[5px] md:bottom-[10px]"
-      } ${isExiting || activeSection ? "bg-white" : "bg-transparent"} ${backgroundTransitionClass}`}
+        activeSection
+          ? "top-[25px] md:top-[50px] h-screen"
+          : "bottom-[5px] md:bottom-[10px]"
+      } ${
+        isExiting || activeSection ? "bg-white" : "bg-transparent"
+      } ${backgroundTransitionClass}`}
     >
-      {/* Navigation avec gestionnaires d'événements pour ouvrir/fermer les sections. */}
       <Nav
         handleSectionClick={handleSectionClick}
         handleCloseClick={handleCloseClick}
         activeSection={activeSection}
         borderClass={borderClass}
       />
-      {/* Contenu de la section active, affiche l'accordéon de projets ou la page À propos. */}
       {activeSection && (
         <main key={activeSection} className="overflow-scroll">
           {activeSection === "Index" && <Accordion items={projects} />}
